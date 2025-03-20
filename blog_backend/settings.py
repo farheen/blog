c8ac662f0b55b7dd6d64d7136c1dc9d8f3701564
Farheen Zahara <farheenzahara@gmail.com>
2025-03-20 18:12:34 +0530

    Fix ALLOWED_HOSTS and incorrect imports

--- blog_backend/settings.py
+++ blog_backend/settings.py
@@ -129,6 +129,7 @@ CORS_ORIGIN_ALLOW_ALL = True
 CORS_ORIGIN_WHITELIST = [
     "http://127.0.0.1:8000",
     "http://127.0.0.1:3000",
+    "blog-backend-n1tx.onrender.com",
 ]
 """
 CORS_ALLOWED_ORIGINS = [
