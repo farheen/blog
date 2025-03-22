ab3afc45d735b35748740acf3a3baece1b93bc78
Farheen Zahara <farheenzahara@gmail.com>
2025-03-22 13:54:05 +0530

    Fixed the backend URl in the front end -3.

--- frontend/src/components/PersonalBlogs.js
+++ frontend/src/components/PersonalBlogs.js
@@ -6,7 +6,7 @@ const PersonalBlogs = () => {
   const [blogs, setBlogs] = useState([]);
 
   useEffect(() => {
-    fetch("${REACT_APP_API_URL}/api/blog/")
+    fetch(`${REACT_APP_API_URL}/api/blog/`)
       .then((response) => response.json())
       .then((data) => setBlogs(data))
       .catch((error) => console.error("Error fetching blogs:", error));
