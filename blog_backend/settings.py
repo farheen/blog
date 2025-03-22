e9f3d99d78785674eb3ef3c5dc94ecdc6ccfffb4
Farheen Zahara <farheenzahara@gmail.com>
2025-03-22 15:33:30 +0530

    Fixed the admin issue - 1.

--- blog_backend/settings.py
+++ blog_backend/settings.py
@@ -28,7 +28,7 @@ ALLOWED_HOSTS = [
 ]
 
 INSTALLED_APPS = [
-    'django.contrib.blog_backend.admin',
+    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'blog_backend.blog.apps.BlogConfig',
     'blog_backend.projects',
     'blog_backend.books',
-    'blog_backend.certificates',
+    'certificates',
     'markdownx',
 ]
 
