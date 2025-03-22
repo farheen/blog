1757d70b9f3828e6ab6937c56346e60eb4b72340
Farheen Zahara <farheenzahara@gmail.com>
2025-03-22 13:10:03 +0530

    Did a data dump, created a data.json file.-3

--- blog_backend/settings.py
+++ blog_backend/settings.py
@@ -5,15 +5,7 @@ import dj_database_url
 
 
 DATABASES = {
-     'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
-     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME','blog'),
-        'USER': os.getenv('DB_USER','postgres'),
-        'PASSWORD': os.getenv('DB_PASSWORD','new_pass'),
-        'HOST': os.getenv('DB_HOST', 'localhost'),
-        'PORT': os.getenv('DB_PORT', '5432'),
-    }
+     'default': dj_database_url.config(default=os.environ.get('DATABASE_URL')),
 }
 
 
