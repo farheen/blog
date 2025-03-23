1a33fe014041dc8c49d33939ef3585dd8ddd7690
Farheen Zahara <farheenzahara@gmail.com>
2025-03-23 12:21:23 +0530

    Fixed the lookup error - 6.

--- blog_backend/certificates/apps.py
+++ blog_backend/certificates/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class CertificatesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'certificates' 
+    label = 'blog_backend.certificates'
