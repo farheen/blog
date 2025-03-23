8e1896def4c701ddb920c2480d885317683f285d
Farheen Zahara <farheenzahara@gmail.com>
2025-03-23 12:07:58 +0530

    Fixed the lookup error - 2.

--- blog_backend/certificates/urls .py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from blog_backend.certificates.views import CertificateListView
-router = DefaultRouter()
-urlpatterns = [
-    path('certificates/', CertificateListView.as_view(), name='CertificateListView'),
-]
-urlpatterns = router.urls
