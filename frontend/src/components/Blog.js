8029b7c64fa31722a161bfcc61db9f6e7d679101
Farheen Zahara <farheenzahara@gmail.com>
2025-04-15 22:12:26 +0530

    Fixed the image size.

--- frontend/src/components/Blog.js
+++ frontend/src/components/Blog.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import ReactMarkdown from "react-markdown";
-
 const apiBaseUrl = process.env.REACT_APP_API_URL;
 
 const Blog = () => {
@@ -22,38 +21,24 @@ const Blog = () => {
             </div>
         );
     }
-
     return (
         <div className="min-h-screen bg-gray-100 p-4">
             <div className="max-w-4xl mx-auto bg-white shadow-md rounded-lg overflow-hidden">
                 {/* Blog Image */}
-                <div className="w-full h-64 bg-gray-200">
+                <div className="w-small h-64                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                              bg-gray-200">
                     <img
                         src={`https://blog-backend-n1tx.onrender.com${blog.image}`}
                         alt={blog.title}
                         className="w-full h-full object-cover"
                     />
                 </div>
-
                 {/* Blog Content */}
                 <div className="p-6">
                     <h1 className="text-3xl font-bold text-gray-800 mb-4">
                         {blog.title}
                     </h1>
-                    <div className="text-gray-600 prose max-w-none">
-                        <ReactMarkdown
-                            components={{
-                                img: ({ node, ...props }) => (
-                                    <img
-                                        {...props}
-                                        className="max-w-full h-auto rounded-lg mx-auto my-4"
-                                        alt={props.alt || ""}
-                                    />
-                                ),
-                            }}
-                        >
-                            {blog.content}
-                        </ReactMarkdown>
+                    <div className="text-gray-600">
+                        <ReactMarkdown>{blog.content}</ReactMarkdown>
                     </div>
                 </div>
             </div>
