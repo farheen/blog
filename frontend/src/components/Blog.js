9040995bb656db06af7d1d453e7d74e7fe25b686
Farheen Zahara <farheenzahara@gmail.com>
2025-04-16 12:23:46 +0530

    Fixed the image.

--- frontend/src/components/Blog.js
+++ frontend/src/components/Blog.js
@@ -29,7 +29,7 @@ const Blog = () => {
                     <img
                         src={`https://blog-backend-n1tx.onrender.com${blog.image}`}
                         alt={blog.title}
-                        className="w-full h-64 bg-gray-200"
+                        className="h-64 bg-gray-200"
                     />
                 </div>
                 {/* Blog Content */}
