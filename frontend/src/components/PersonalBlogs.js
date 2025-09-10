f8d8cc45ae988f075fec5e2f8573efeeddcbad01
Farheen Zahara <farheenzahara@gmail.com>
2025-09-10 21:46:37 +0530

    Added images to the blog list tile.

--- frontend/src/components/PersonalBlogs.js
+++ frontend/src/components/PersonalBlogs.js
@@ -28,7 +28,9 @@ const PersonalBlogs = () => {
                 <CardActionArea component={Link} to={`/blogs/${blog.id}`}>
                   <CardContent>
                     <Typography variant="h5" gutterBottom color="primary">
+                      {blog.image}
                       {blog.title}
+                     }
                     </Typography>
                     <Typography variant="body2" color="textSecondary">
                       {blog.abstract || blog.content.substring(0, 150) + "..."}
