e712f1bd0b68be713db83fbc2faf6dad9510c457
Farheen Zahara <farheenzahara@gmail.com>
2025-09-15 19:09:57 +0530

    changed the ProjectTile.js

--- frontend/src/components/ProjectTile.js
+++ frontend/src/components/ProjectTile.js
@@ -2,17 +2,33 @@ import React from "react";
 import "../css/ProjectTile.css";
 const apiBaseUrl = process.env.REACT_APP_API_URL;
 
-const ProjectTile = ({ title, image, code_url, blog_url }) => {
+const ProjectTile = ({ title, image, code_url, blog_url, embed_url }) => {
   const backendUrl = `${apiBaseUrl}`;
   const imageUrl = image ? `${backendUrl}${image}` : null;
 
   return (
     <div className="card h-100">
-      {imageUrl && (
-        <img src={imageUrl} className="card-img-top" />
-      )}
+      {/* Thumbnail (optional) */}
+      {imageUrl && <img src={imageUrl} className="card-img-top" alt={title} />}
+
       <div className="card-body">
         <h5 className="card-title">{title}</h5>
+
+        {/* Embedded Power BI Dashboard */}
+        {embed_url && (
+          <div className="mb-3">
+            <iframe
+              title={title}
+              width="100%"
+              height="300"
+              src={embed_url}
+              frameBorder="0"
+              allowFullScreen={true}
+            ></iframe>
+          </div>
+        )}
+
+        {/* Action buttons */}
         <div className="d-flex justify-content-between">
           {code_url && (
             <a
